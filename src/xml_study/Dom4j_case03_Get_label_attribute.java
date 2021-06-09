692c11fb5d27aa73be19eaf3be6b276a41982df5
Mrzxs999 <mrzxs999@163.com>
2021-06-09 10:07:07 +0800

    dom4j operate xml file update

---
+++ src/xml_study/Dom4j_case03_Get_label_attribute.java
@@ -0,0 +1,18 @@
+package xml_study;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class Dom4j_case03_Get_label_attribute {
+    public static void main(String[] args) throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Document read = saxReader.read("src/xml_study/tt.xml");
+        Element rootElement = read.getRootElement();
+        Element user1 = rootElement.element("user1");
+        String id = user1.attributeValue("id");
+        System.out.println(id);
+    }
+}
