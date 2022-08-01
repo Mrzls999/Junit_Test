f7aa3872954bcf676706bfd96ffb209ee5c10090
zls <mrzxs999@163.com>
2022-08-01 20:58:11 +0800

    对于文件第二行替换成文件的字节数

---
+++ src/zlsUtils/ReplaceLine2.java
@@ -0,0 +1,15 @@
+package zlsUtils;
+
+/**
+ * @author zls
+ * @date 2022/8/1 20:43:28
+ * @description 对于文件的第二行替换成文件的字节数
+ */
+public class ReplaceLine2 {
+    public static void main(String[] args) {
+        //0、输入压缩文件所处的文件夹路径
+        String dirPath = "";
+        //1、获取此文件夹下的所有.zip结尾的文件
+
+    }
+}
