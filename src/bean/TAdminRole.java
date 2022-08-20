e5f786d0e6f1053a691a68b7bd34a1a88a63d7bc
zls <mrzxs999@163.com>
2022-08-20 15:16:39 +0800

    add something

---
+++ src/bean/TAdminRole.java
@@ -0,0 +1,15 @@
+package bean;
+
+/**
+ * @author zls
+ * @date 2022/8/20 15:15:52
+ * @description XXX
+ */
+public class TAdminRole {
+    private Long id;
+
+    private Long adminid;
+
+    private Long roleid;
+
+}
