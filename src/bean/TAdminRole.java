8442146fd4db2d53d2a978c140ed83112b9e988a
zls <mrzxs999@163.com>
2022-08-20 15:17:11 +0800

    update TAdminRole的function

--- src/bean/TAdminRole.java
+++ src/bean/TAdminRole.java
@@ -12,4 +12,28 @@ public class TAdminRole {
 
     private Long roleid;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAdminid() {
+        return adminid;
+    }
+
+    public void setAdminid(Long adminid) {
+        this.adminid = adminid;
+    }
+
+    public Long getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(Long roleid) {
+        this.roleid = roleid;
+    }
+
 }
