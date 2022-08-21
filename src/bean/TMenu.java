b65ee11155ef41fec00ce7f15ce133057b6c8921
zls <mrzxs999@163.com>
2022-08-21 18:41:32 +0800

    update bean of menu

--- src/bean/TMenu.java
+++ src/bean/TMenu.java
@@ -1,11 +1,26 @@
 package bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author zls
  * @date 2022/8/21 18:40:31
  * @description XXX
  */
 public class TMenu {
+
+    private Long id;
+
+    private Long pid;
+
+    private String name;
+
+    private String icon;
+
+    private String url;
+
+    private List<TMenu> childMenus = new ArrayList<>();
 
 
 }
