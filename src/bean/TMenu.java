31c1c03bfb5d58ad7de8a63894549b087c01892e
zls <mrzxs999@163.com>
2022-08-21 18:41:58 +0800

    update bean of menu by some functions

--- src/bean/TMenu.java
+++ src/bean/TMenu.java
@@ -22,5 +22,58 @@ public class TMenu {
 
     private List<TMenu> childMenus = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public List<TMenu> getChildMenus(){return childMenus;}
+
+    @Override
+    public String toString() {
+        return "TMenu{" +
+                "id=" + id +
+                ", pid=" + pid +
+                ", name='" + name + '\'' +
+                ", icon='" + icon + '\'' +
+                ", url='" + url + '\'' +
+                ", childMenus=" + childMenus +
+                '}';
+    }
 
 }
