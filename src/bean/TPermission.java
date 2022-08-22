e9d1b35492a77a329d2d5a410339ba00ece3d309
zls <mrzxs999@163.com>
2022-08-22 23:06:12 +0800

    add class of TPermission and add some files

---
+++ src/bean/TPermission.java
@@ -0,0 +1,21 @@
+package bean;
+
+/**
+ * @author zls
+ * @date 2022/8/22 23:05:19
+ * @description XXX
+ */
+public class TPermission {
+
+    private Long id;
+
+    private String name;
+
+    private String title;
+
+    private String icon;
+
+    private Long pid;
+
+
+}
