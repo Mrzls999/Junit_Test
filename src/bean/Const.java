08b44ca2daf5a514c2319b0213ca52f0cd54bbfc
zls <mrzxs999@163.com>
2022-08-23 20:51:45 +0800

    update utils bean Const

--- src/bean/Const.java
+++ src/bean/Const.java
@@ -7,5 +7,9 @@ package bean;
  */
 public class Const {
     public static final String DEFALUT_PASSWORD = "123";
+    public static final String PATH = "PATH";
+    public static final String LOGIN_ADMIN = "loginAdmin";
 
+    public static final String LOGIN_LOGINACCT_ERROR = "用户名称不存在!";
+    public static final String LOGIN_USERPSWD_ERROR = "用户密码不正确!";
 }
