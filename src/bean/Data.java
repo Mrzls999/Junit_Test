0b9f1c3af1bc7de88e8f7b145da7b27a38410dde
zls <mrzxs999@163.com>
2022-08-24 08:51:18 +0800

    ss

--- src/bean/Data.java
+++ src/bean/Data.java
@@ -10,5 +10,14 @@ import java.util.List;
  */
 public class Data {
 
+    private List<Integer> ids = new ArrayList<Integer>();
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
 
 }
